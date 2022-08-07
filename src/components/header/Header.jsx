acee2e914ca621e04be38fa42e6deaf2c3e17159
alikh157 <ali.khakbaz134878@gmail.com>
2022-08-07 21:40:11 +0430

    16 mordad

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -5,18 +5,21 @@ import Button from '@mui/material/Button';
 import ticketImage from './images/boarding_pass_60px.png';
 import {FontAwesomeIcon} from '@fortawesome/react-fontawesome';
 import {solid, regular, brands} from '@fortawesome/fontawesome-svg-core/import.macro';
-import {makeStyles} from "@mui/material";
-// import classes from "*.module.css";
+import {makeStyles} from "@mui/styles";
+
 const useStyles = makeStyles({
     login: {
         marginTop: '5px',
         marginLeft: '5px',
-        '&.MuiButton-outlinedSecondary': {
-            border: '1px solid pink',
+        '&.MuiButton-textSecondary': {
+            borderRadius: '10px',
+            color: '#6c6464',
+            fontFamily: 'IRANSansX'
         },
     },
 });
 const Header = () => {
+    const classes = useStyles();
     return (
         <header>
             <img className={"logo"} src={logo} alt="logo" height={"70px"}/>
@@ -29,20 +32,26 @@ const Header = () => {
                 </ul>
             </nav>
             <div className="login">
-                    <Button
-                        variant="outlined"
-                        color="secondary"
-                        size={"medium"}
-                        className={useStyles.login}
-                    > ورود/ثبتنام</Button>
-                    {/*<img src={ticketImage} alt="" style={{*/}
-                    {/*    color:"red",*/}
-                    {/*    background_color:"red"*/}
-                    {/*}}/>*/}
-                    {/*<FontAwesomeIcon icon={solid('ticket-airline')}/>*/}
+                <Button
+                    variant="text"
+                    color="secondary"
+                    href="/login"
+                    size={"large"}
+                    className={classes.login}
+                > ورود/ثبتنام <FontAwesomeIcon className={'icon'} style={{"transform": 'rotate(180deg)'}} icon={solid('arrow-right-to-bracket')}/></Button>
             </div>
         </header>
     );
 };
+{/*<img src={ticketImage} alt="" style={{*/
+}
+{/*    color:"red",*/
+}
+{/*    background_color:"red"*/
+}
+{/*}}/>*/
+}
+{/*<FontAwesomeIcon icon={solid('ticket-airline')}/>*/
+}
 
 export default Header;
